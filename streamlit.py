a97ac2e3ce11a0c0ac4762d5ee5088e3f717954a
1997bugs <40323112+1997bugs@users.noreply.github.com>
2022-07-27 20:20:10 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from plugnplay import *
+#from plugnplay import *
 
 st.set_page_config(
     page_title="Review Categories",
@@ -11,11 +11,11 @@ comment = st.text_input('Enter comment','This is good')
 st.write('Comment is ',comment)
 
 text_inp = comment
-ps = cmt_pos2_edit(text_inp)
-fl,psble = sim_bucketing(ps,model,buckets)
+#ps = cmt_pos2_edit(text_inp)
+#fl,psble = sim_bucketing(ps,model,buckets)
 
 st.write("Your Input Text : ",text_inp)
 #print("Word pairs captured:",ps)
 st.write("##### Output #####")
-st.write("Major themes:",fl)
-st.write("Next closest pairs:",psble)
+#st.write("Major themes:",fl)
+#st.write("Next closest pairs:",psble)
